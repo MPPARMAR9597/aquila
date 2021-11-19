c6d1b68a7b0d035926bdfedafc740126bf216b3e
M.P <mkparmar41@gmail.com>
2021-11-19 19:13:45 +0530

    Create header | Footer | Custom Theme Snippets

---
+++ footer.php
@@ -0,0 +1,22 @@
+<?php 
+
+/**
+ * Footer file.
+ * 
+ * @package Aquila
+ */
+
+?>
+
+<footer>Footer For Our Theme </footer>
+
+
+</div>
+<!-- End Div Of #page And Class="site-content" -->
+</div>
+<!-- End Div Of #content And Class="site" -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -8,15 +8,17 @@
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wordpress Theme</title>
-</head>
-<body>
-    
-</body>
-</html>
+<!-- Two Types to include Header -->
+
+<?php // include_once 'header.php'; ?>
+
+<?php get_header(); ?>
+    <div class="content">
+        content
+    </div>
+
+<!-- Two Types to include Footer -->
+
+<?php // include_once 'footer.php'; ?>
+
+<?php get_footer();
